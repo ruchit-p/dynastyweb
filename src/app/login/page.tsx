39d0105a54de021cee836bbadfd680b77fd7387c
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2025-03-01 20:38:32 -0500

    Updated auth tree image to link back to root

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -85,13 +85,15 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          src="/dynasty.png"
-          alt="Dynasty Logo"
-          width={60}
-          height={60}
-          className="mx-auto"
-        />
+        <Link href="/">
+          <Image
+            src="/dynasty.png"
+            alt="Dynasty Logo"
+            width={60}
+            height={60}
+            className="mx-auto"
+          />
+        </Link>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Sign In to Dynasty
         </h2>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -175,13 +175,15 @@ export default function SignupPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          src="/dynasty.png"
-          alt="Dynasty Logo"
-          width={60}
-          height={60}
-          className="mx-auto"
-        />
+        <Link href="/">
+          <Image
+            src="/dynasty.png"
+            alt="Dynasty Logo"
+            width={60}
+            height={60}
+            className="mx-auto"
+          />
+        </Link>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Create Your Account
         </h2>
