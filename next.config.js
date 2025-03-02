5512257d466cd40774f3e38c2d3ce91bbecca7da
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2025-03-02 16:48:45 -0500

    updated config

--- next.config.js
+++ next.config.js
@@ -26,10 +26,10 @@ const nextConfig = {
 }
 
 // Injected content via Sentry wizard below
-const { withSentryConfig } = require("@sentry/nextjs");
+import { withSentryConfig } from "@sentry/nextjs";
 
 // Export the final config with Sentry applied
-module.exports = withSentryConfig(
+export default withSentryConfig(
   nextConfig,
   {
     // For all available options, see:
